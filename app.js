cfec796340fa97cedfc704f51d60238b8bf98860
jawantechpk <jawantechpk@gmail.com>
2021-11-15 12:28:22 +0500

    15/11/21

---
+++ app.js
@@ -0,0 +1,240 @@
+// alert("HELLO CLASS....")
+
+// var days =  ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// var nowDay = new Date().getDay()
+
+// document.write("TODAY IS" + days[nowDay])
+
+
+
+
+// console.log(nowDay)
+
+
+
+// var now =  new Date().getTime()
+// console.log(now)
+
+// var courseStart = new Date("Aug 09, 2021").getTime();
+// console.log(courseStart);
+
+
+// var msDiff = now - courseStart
+
+// var totalDays =Math.floor(  msDiff / (1000 * 60 * 60 *24)) 
+
+// console.log(totalDays);
+
+
+
+// var now = new Date()
+
+// console.log(now.getHours() - 1)
+
+// var setHours = new Date().setHours( now.getHours() - 1 )
+// console.log(setHours);  
+
+// var currentHOur = new Date().getTime()
+
+
+// var diff = currentHOur - setHours
+
+// console.log( Math.floor(diff /( 1000   )));
+
+
+
+
+
+        /////FUNCTIONs///
+
+
+// alert("HELLO CLASS");
+
+
+// function  function_name (  ) {
+
+
+
+// }
+
+
+// function foo(){
+
+    
+//     alert("foo")
+
+
+
+// }
+
+// function foo2(){
+
+//     alert("FOO2")
+
+
+// }
+
+
+
+
+// foo2()
+// foo2()
+// foo2()
+// foo2()
+// foo2()
+// foo2()
+// foo2()
+// foo2()
+// foo2()
+// // foo()
+
+
+
+
+// function add(  num1 , num2  , value ){
+
+//         alert(  value + (num1 + num2) )
+
+
+// }
+
+
+// var num1 = +prompt("ENTE NUM 1")
+// var num2 = +prompt("ENTE NUM 2")
+
+// add( num1 , num2 , "ADDITIOn"  )
+
+
+
+
+// function add( n1 , n2 ){
+
+
+//     return  n1 + n2
+
+
+// }
+
+// // var result = add(20 , 30)
+
+// console.log(    add(20 , 30)    );
+
+
+
+// function adddition(num1 , num2){
+
+//     var value = 500  + num1 + num2
+
+//     // alert(value); 
+
+//     return value
+
+
+// }
+
+
+// console.log(adddition(50 , 80))
+// // adddition(50 , 80)
+
+
+
+// function  value(){
+
+//         var value = 500;
+        
+//         return value
+
+
+// }
+
+
+// var value1 = value()  + 100
+
+// console.log(value1);
+
+
+
+// function foo(num1  , num2 = 0 ){
+
+//     alert(num1 + num2)        
+        
+       
+
+    
+//     }
+
+
+
+// foo(200)
+
+
+
+
+
+
+// var names = "JAFFAR";
+
+// function foo(){
+
+//     names = "JAFFAR AMAN";
+    
+//     console.log(names);
+
+
+// }
+
+// // console.log(names);
+
+// foo()
+
+
+
+/*var value = "12345"
+
+function foo(){
+    value = "123456789"
+    console.log( "LOCAL",value);
+}
+
+
+
+
+foo()
+
+
+
+console.log( "GLOBAL",value);
+*/
+
+
+
+
+function foo( num1 , num2 ){
+
+    alert(num1 + num2)
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
