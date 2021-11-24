72b4d57c0b5ce7b7920f58803e7b85c3f3f0e986
Jaffar Aman <65033254+JaffarAman@users.noreply.github.com>
2021-11-24 12:07:26 +0500

    DOM first Class 24/11/21
    DOM first Class 24/11/21

---
+++ class 5/app.js
@@ -0,0 +1,110 @@
+// alert("HELLO CLASS...");
+
+
+// var age = 25;
+
+// switch( age ) 
+// {
+//     //statement////
+//     case 20:
+//     console.log("age is match");
+//     break
+//     default:
+//     console.log("not match...")
+//     break
+
+// }
+
+
+
+// var age = +prompt("Enter your age..")
+
+// switch(age){
+//     case 20:
+//     alert("allow");
+//     break;
+//     case 25:
+//     alert("allow");
+//     break;
+//     case 30:
+//     alert("allow");
+//     break;
+//     default :
+//     alert("not allow")
+//     break;
+
+
+// }
+
+
+    ////while loop  & do while loop////
+
+// var userInd = prompt()
+
+// for (var index = userInd; index < array.length; index++) {
+//     var element = array[index];
+    
+// }
+
+
+
+
+// ///1st step
+// var i = 1;
+
+// ///2nd step
+// while( i <= 10){
+
+//     ///statements start///
+//     console.log(i)
+//     ///statements END///
+
+
+
+
+//     //3rd step
+//     i++
+
+// }
+
+
+////DO WHILE LOOP////
+
+
+// var i = 0
+// do{
+//     console.log("do while")
+//     i++
+// }while( i < 0 );
+
+
+
+    //the dom///
+
+// var a  = document.childNodes[1].childNodes
+
+// console.log(a)
+
+
+// var htmlTag = document.childNodes[1].childNodes
+
+// console.log(htmlTag)
+
+
+// var pappa = document.getElementById("pappa")
+// // console.log(pappa.childNodes)
+
+// // console.log(pappa.lastChild)
+
+
+// var firstChild = pappa.firstChild
+// console.log(firstChild.firstChild);
+// // var lastChild = pappa.lastChild
+
+
+// //mid child
+// console.log(pappa.lastChild.previousSibling)
+
+
+var para = document.getElementById("para");
+console.log(para.lastChild);
